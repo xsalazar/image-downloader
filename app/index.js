2924eaef991893edb8d7c11bf941b80733bbe611
Xavier Salazar <x.salazar1729@gmail.com>
2022-04-27 23:12:27 -0700

    Add initial implementation

---
+++ app/index.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+
+exports.handler = async (event, context) => {
+  console.log(JSON.stringify(event));
+
+  var errorReturnResponse = {
+    cookies: [],
+    isBase64Encoded: false,
+    statusCode: 400,
+    headers: {},
+    body: "",
+  };
+
+  if (event.queryStringParameters) {
+    if (!event.queryStringParameters.imageSource) {
+      return errorReturnResponse;
+    }
+
+    var imageSource = event.queryStringParameters.imageSource;
+
+    if (
+      !imageSource.startsWith(
+        "https://www.gstatic.com/android/keyboard/emojikitchen"
+      )
+    ) {
+      return errorReturnResponse;
+    }
+
+    // Get image from internet
+    var response = await axios.get(imageSource, {
+      responseType: "blob",
+    });
+
+    return {
+      cookies: [],
+      isBase64Encoded: true,
+      statusCode: 200,
+      body: response.data,
+    };
+  }
+};
